fae4eba9045988a8cc7a213cba528305e2058b0e
Exenreco Bell <exenreco19@yahoo.com>
2025-01-16 12:31:28 -0600

    Project: Hands On 1.1 - Complete

---
+++ week_2/project01_01/project01_01.py
@@ -0,0 +1,9 @@
+"""
+Author: Exenreco Bell
+Date: January 16, 2025
+File Name: project01-01.py
+Description: This program prints a motivational quote and credits the author.
+"""
+
+# Using the print function to display a quote
+print(f"The only way to do great work is to love what you do. - Steve Jobs")
