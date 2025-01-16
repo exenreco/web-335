3d04b0f43722dbd070aa026cf36d2fd09779cd91
Exenreco Bell <exenreco19@yahoo.com>
2025-01-16 14:14:26 -0600

    Project: Hands On 2.1 - Complete

--- week_2/hands_on_01_01/hands_on_01_01.py
+++ week_2/hands_on_01_01/hands_on_01_01.py
@@ -1,7 +1,7 @@
 """
 Author: Exenreco Bell
 Date: January 16, 2025
-File Name: project01_01.py
+File Name: hands_on_01_01.py
 Description: This program prints a motivational quote and credits the author.
 """
 
